91fc12e3f917a726b127ddff9b0d1c26ff4c998a
Min Min Latt <minminlaxz@gmail.com>
2024-07-06 18:10:48 +0630

    Update setup.sh

--- reverse-proxy/setup.sh
+++ reverse-proxy/setup.sh
@@ -2,7 +2,10 @@
 set -e
 
 docker network inspect proxy-net > /dev/null 2>&1 || docker network create proxy-net
+
 echo "Enter your default host for nginx proxy server (e.g. anything.example.com):"
 read DEFAULT_HOST
+
 yq w -i ./docker-compose.yml services.nginx-proxy.environment[1] "DEFAULT_HOST=$DEFAULT_HOST"
-docker-compose -f ./docker-compose.yml up -d
+
+docker-compose up -d
